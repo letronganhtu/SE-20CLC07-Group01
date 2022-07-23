bfcacf00c81184cee947537b2e2eed5665dfb02b
Le Trong Anh Tu <letronganhtu@gmail.com>
2022-07-23 12:37:42 +0700

    Update source code of project

---
+++ src/2nd Edition/main.py
@@ -0,0 +1,15 @@
+from Server.server import *
+import socket
+
+if __name__ == '__main__':
+    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.connect(('8.8.8.8', 80))
+    #PATH = s.getsockname()[0]
+    PATH = "localhost"
+    PORT = 8080
+    print('------------------------------------------------------------')
+    print("Connect to '{}:{}/' to access the website.".format(str(PATH), str(PORT)))
+    print('------------------------------------------------------------\n')
+
+    httpd = HTTPServer((PATH, PORT), Server)
+    httpd.serve_forever()
